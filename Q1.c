d6601ff5b8132f2753c9200fab5d9688412f6b9e
PARTH KAUL <pkaul036@gmail.com>
2025-08-28 23:40:46 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter a integer ");
+    scanf("%d", &a);
+    printf("enter a integer ");
+    scanf("%d", &b);
+    int c,
+    c = a + b;
+    printf("the sum is %d", c);
+    return 0;
+}
