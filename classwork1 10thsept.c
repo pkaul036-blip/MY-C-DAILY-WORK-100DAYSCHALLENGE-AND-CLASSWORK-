d0e7c8fe727ad778cc4ae98faff7af21f52f12ae
PARTH KAUL <pkaul036@gmail.com>
2025-09-10 10:34:33 +0530

    Create classwork1 10thsept.c

---
+++ classwork1 10thsept.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Array in reverse order:\n");
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
